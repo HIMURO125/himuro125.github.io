0eecb05c8dfefc473f9f966d44ee8bd8f4d0c25b
HIMURO125 <himuro125@gmail.com>
2024-09-03 14:28:26 +0900

    Unity WebGL
    更新

--- Build/BuildWeb.loader.js
+++ Build/BuildWeb.loader.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86ea62283e54262ba471d7fccfd1af877ef7a09ec4d047afbd9ddbb6d315d169
-size 20642
+oid sha256:1d61ac36c38d8fa61f2e2e53974a6047ccc321d79b216b068930afc6a205ff76
+size 21807
